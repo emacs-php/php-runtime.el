3938627fa08a336f5f8213d3bc68e93fca76f348
USAMI Kenta <tadsan@zonu.me>
2017-08-27 13:34:17 +0900

    Improve functions

--- php-runtime.el
+++ php-runtime.el
@@ -57,24 +57,88 @@
 (defvar php-runtime--kill-temp-output-buffer t)
 
 
+;; Utility functions
+(defun php-runtime--temp-buffer ()
+  "Return new temp buffer."
+  (generate-new-buffer "*PHP temp*"))
+
+(defun php-runtime--stdin-satisfied-p (obj)
+  "Return t if the object `OBJ' is satisfied to stdin format.
+
+for example, (get-buffer \"foo-buffer\"), '(:file . \"/path/to/file\")."
+  (cond
+   ((null obj) t)
+   ((and (bufferp obj) (buffer-live-p obj) t))
+   ((and (consp obj)
+         (eq :file (car obj)) (stringp (cdr obj))) t)))
+
+(defun php-runtime--code-satisfied-p (obj)
+  "Return t if the object `OBJ' is satisfied to code format."
+  (and (consp obj)
+       (member (car obj) (list :file :string))
+       (stringp (cdr obj))))
+
+
 ;; PHP Execute class
 
 ;;;###autoload
 (defclass php-runtime-execute nil
   ((executable :initarg :executable :type string)
-   (code   :initarg :code   :type (or null string) :initform nil)
-   (stdin  :initarg :stdin  :type (or null buffer) :initform nil)
-   (stdout :initarg :stdout :type (or null buffer list) :initform nil)
-   (stderr :initarg :stderr :type (or null buffer list) :initform nil)))
+   (code   :initarg :code   :type (satisfies php-runtime--code-satisfied-p))
+   (stdin  :initarg :stdin  :type (satisfies php-runtime--stdin-satisfied-p) :initform nil)
+   (stdout :initarg :stdout :type (or null buffer-live list) :initform nil)
+   (stderr :initarg :stderr :type (or null buffer-live list) :initform nil)))
 
-(defmethod php-runtime-run-in-command-line ((php php-runtime-execute))
-  "Execute PHP process."
-  (call-process (oref php executable)
-                (oref php stdin)
-                (cons (php-runtime-stdout-buffer php)
-                      (oref php stderr))
-                nil ; suppress display
-                (concat "-r" (oref php code))))
+(defmethod php-runtime-run ((php php-runtime-execute))
+  "Execute PHP process using `php -r' with code.
+
+This execution method is affected by the number of character limit of OS command arguments.
+You can check the limitation by command, for example \(shell-command-to-string \"getconf ARG_MAX\") ."
+  (let ((args (list (php-runtime--get-command-line-arg php))))
+    (if (and (oref php stdin) (not (php-runtime--stdin-by-file-p php)))
+        (php-runtime--call-php-process-with-input-buffer php args)
+      (php-runtime--call-php-process php args))))
+
+(defmethod php-runtime--call-php-process ((php php-runtime-execute) args)
+  "Execute PHP Process by php-execute `PHP' and `ARGS'."
+  (apply #'call-process (oref php executable)
+         (php-runtime--get-input php) ;input
+         (cons (php-runtime-stdout-buffer php)
+               (oref php stderr))
+         nil ; suppress display
+         args))
+
+(defmethod php-runtime--call-php-process-with-input-buffer ((php php-runtime-execute) args)
+  "Execute PHP Process with STDIN by php-execute `PHP' and `ARGS'."
+  (unless (buffer-live-p (oref php stdin))
+    (error "STDIN buffer is not available"))
+  (with-current-buffer (oref php stdin)
+    (apply #'call-process-region (point-min) (point-max)
+           (oref php executable)
+           nil ;delete
+           (cons (php-runtime-stdout-buffer php)
+                 (oref php stderr))
+           nil ; suppress display
+           args)))
+
+(defmethod php-runtime--get-command-line-arg ((php php-runtime-execute))
+  "Return command line string"
+  (let ((code (oref php code)))
+    (cl-case (car code)
+      (:file (cdr code))
+      (:string (concat "-r" (cdr code))))))
+
+(defmethod php-runtime--stdin-by-file-p ((php php-runtime-execute))
+  "Return T if \(oref php stdin) is file."
+  (let ((stdin (oref php stdin)))
+    (and (consp stdin)
+         (eq :file (car stdin)))))
+
+(defmethod php-runtime--get-input ((php php-runtime-execute))
+  ""
+  (if (php-runtime--stdin-by-file-p php)
+      (cdr (oref php stdin))
+    nil))
 
 (defmethod php-runtime-stdout-buffer ((php php-runtime-execute))
   "Return output buffer."
@@ -82,12 +146,6 @@
     (if (and buf (buffer-live-p buf))
         buf
       (oset php stdout (generate-new-buffer "*PHP output*")))))
-
-
-;; Utility functions
-(defun php-runtime--temp-buffer ()
-  "Return new temp buffer."
-  (generate-new-buffer "*PHP temp*"))
 
 
 ;; PHP Execute wrapper function
@@ -104,18 +162,25 @@ Pass `INPUT-BUFFER' to PHP executable as STDIN."
   "Evalute PHP code `CODE' without open tag, and return buffer.
 
 Pass `INPUT-BUFFER' to PHP executable as STDIN."
-  (let ((execute (php-runtime-execute :code code
+  (let ((execute (php-runtime-execute :code (cons :string code)
                            :executable php-runtime-php-executable
-                           :stderr (get-buffer-create php-runtime-error-buffer-name))))
+                           :stderr (get-buffer-create php-runtime-error-buffer-name)))
+        (temp-input-buffer (when (and input-buffer (not (bufferp input-buffer)))
+                             (php-runtime--temp-buffer))))
     (when input-buffer
-      (oset execute stdin input-buffer))
+      (oset execute stdin
+            (if (or (bufferp input-buffer)
+                    (and (consp input-buffer) (eq :file (car input-buffer))))
+                input-buffer
+              (with-current-buffer temp-input-buffer
+                (insert input-buffer)))))
 
     (unwind-protect
-        (progn (php-runtime-run-in-command-line execute)
+        (progn (php-runtime-run execute)
                (with-current-buffer (php-runtime-stdout-buffer execute)
                  (buffer-substring-no-properties (point-min) (point-max))))
-      (when (and input-buffer (buffer-live-p input-buffer))
-        (kill-buffer input-buffer))
+      (when (and temp-input-buffer (buffer-live-p temp-input-buffer))
+        (kill-buffer temp-input-buffer))
       (when php-runtime--kill-temp-output-buffer
         (kill-buffer (php-runtime-stdout-buffer execute))))))
 
